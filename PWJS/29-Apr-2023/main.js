cccb8c2b48a997dca59b9e3912755843827aec22
Syed Murtaza Hussain <sirmurtazaaptechtr@outlook.com>
2023-05-02 20:38:52 +0500

    02-May-2023

--- PWJS/29-Apr-2023/main.js
+++ PWJS/29-Apr-2023/main.js
@@ -6,15 +6,24 @@ showNotes();
 let addBtn = document.getElementById("addBtn");
 addBtn.addEventListener("click", function(e) {
   let addTxt = document.getElementById("addTxt");
+  let noteTitle = document.getElementById("noteTitle");
+  let priority = document.getElementsByName("priority");
+  let pValue;
+  for (i = 0; i < priority.length; i++) {
+    if (priority[i].checked){
+      pValue = priority[i].value;
+    }
+  }
   let notes = localStorage.getItem("notes");
   if (notes == null) {
     notesObj = [];
   } else {
     notesObj = JSON.parse(notes);
   }
-  notesObj.push(addTxt.value);
+  notesObj.push({text:addTxt.value,title:noteTitle.value,bgColor:pValue});
   localStorage.setItem("notes", JSON.stringify(notesObj));
   addTxt.value = "";
+  noteTitle.value = "";
 //   console.log(notesObj);
   showNotes();
 });
@@ -30,11 +39,11 @@ function showNotes() {
   let html = "";
   notesObj.forEach(function(element, index) {
     html += `
-            <div class="noteCard my-2 mx-2 card" style="width: 18rem;">
+            <div class="noteCard my-2 mx-2 card ${element.bgColor}" style="width: 18rem;">
                     <div class="card-body">
-                        <h5 class="card-title">Note ${index + 1}</h5>
-                        <p class="card-text"> ${element}</p>
-                        <button id="${index}"onclick="deleteNote(this.id)" class="btn btn-primary">Delete Note</button>
+                        <h5 class="card-title">${element.title} <span class="badge bg-secondary text-light">#note${index+1}</span></h5>
+                        <p class="card-text"> ${element.text}</p>
+                        <button id="${index}"onclick="deleteNote(this.id)" class="btn btn-secondary">Delete Note</button>
                     </div>
                 </div>`;
   });
